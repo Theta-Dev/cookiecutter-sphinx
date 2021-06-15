d2a9c369f6e2f8c90924f5325c76c565e680222f
ThetaDev <t.testboy@gmail.com>
2021-06-15 16:21:05 +0200

    Updated exclude patterns

--- {{cookiecutter.dir_name}}/conf.py
+++ {{cookiecutter.dir_name}}/conf.py
@@ -33,7 +33,7 @@ templates_path = ['_templates']
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['*/*', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ['.tox', 'venv', '_build', 'Thumbs.db', '.DS_Store']
 
 # Pygments-Styling used for code syntax highlighting.
 # See this page for an overview of all styles including live demo:
